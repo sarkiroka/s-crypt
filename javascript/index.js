93f4d18d36dbc7ae426e91872e0a585414f14335
sarkiroka <sarki_roka@freemail.hu>
2017-01-01 23:23:18 +0100

    refaktor

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,15 @@
 /**
  * @author sarkiroka
  */
+var encode = require('./encode');
+var decode = require('./decode');
 module.exports = function (password) {
 	if (!password) {
 		throw new Error('no password set');
 	}
 	var base64Password = new Buffer(password).toString('base64');
 	return {
-		encode: require('./encode')(base64Password),
-		decode: require('./decode')(base64Password)
+		encode: encode(base64Password),
+		decode: decode(base64Password)
 	};
 };
